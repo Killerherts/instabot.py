a436e745070f33151d5df316a8695042e596667e
fv4jhro <karnal02@gmail.com>
2016-11-16 15:17:50 +0100

    Create validation for user exist

--- src/instabot.py
+++ src/instabot.py
@@ -183,21 +183,27 @@ class InstaBot:
         self.write_log(log_string)
         self.login()
         self.populate_user_blacklist()
-        signal.signal(signal.SIGTERM, self.cleanup)
-        atexit.register(self.cleanup)
+        # signal.signal(signal.SIGTERM, self.cleanup)
+        # atexit.register(self.cleanup)
 
     def populate_user_blacklist(self):
         for user in self.user_blacklist:
 
-            user_id_url= self.url_user_detail % (user)
+            user_id_url = self.url_user_detail % (user)
             info = self.s.get(user_id_url)
-            all_data = json.loads(info.text)
-            id_user = all_data['user']['media']['nodes'][0]['owner']['id']
-            #Update the user_name with the user_id
-            self.user_blacklist[user]=id_user
-            log_string = "Blacklisted user %s added with ID: %s" % (user, id_user)
-            self.write_log(log_string)
-            time.sleep(5 * random.random())
+            # Valiaditon if user exists
+            if info.status_code == 404:
+                # if not exists show test
+                log_string = "User {} from the blacklist does not exist".format(user)
+            else:
+                # f user exists continue normal
+                all_data = json.loads(info.text)
+                id_user = all_data['user']['media']['nodes'][0]['owner']['id']
+                #Update the user_name with the user_id
+                self.user_blacklist[user]=id_user
+                log_string = "Blacklisted user %s added with ID: %s" % (user, id_user)
+                self.write_log(log_string)
+                time.sleep(5 * random.random())
 
         log_string = "Completed populating user blacklist with IDs"
         self.write_log(log_string)
